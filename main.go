a277b08c1302bb1548a7fa3c872b657e479f960f
Marius <marlil@stud.ntnu.no>
2018-10-12 15:33:12 +0200

    loads of changes, bunch of errors

--- main.go
+++ main.go
@@ -1,82 +1,171 @@
 package main
 
 import (
-	"errors"
+	"encoding/json"
 	"fmt"
 	"github.com/marni/goigc"
-	"html/template"
-	"io/ioutil"
-	"log"
+	"math/rand"
 	"net/http"
 	"regexp"
+	"strconv"
+	"strings"
 	"time"
 )
 
+
 var startTime time.Time
-var templates = template.Must(template.ParseFiles("edit.html", "view.html", "apiTemplate.html"))
-var validPath = regexp.MustCompile("^/igcinfo/(api)/$|\0121(igc)/$|\0122([0-9][0-9][0-9][0-9])/$|\0123(pilot|glider_id|glider|calculated_total_track_length|H_date)/$")
+var tracks map[int]string
+var trackinf map[int]TrackInfo
+var numTracks int
+//var validPath = regexp.MustCompile("^/igcinfo/(api)/$|\0121(igc)/$|\0122([0-9][0-9][0-9][0-9])/$|\0123(pilot|glider_id|glider|calculated_total_track_length|H_date)/$")
+var validField = regexp.MustCompile("^(pilot|glider_id|glider|calculated_total_track_length|H_date)$")
 
-type Page struct {
-	Title string
-	Body  []byte
+
+type MetaData struct {
+	Uptime string `json:"uptime"`
+	Info string `json:"info"`
+	Version string `json:"version"`
 }
 
-type data struct {
-	uptime int `json:"uptime"`
-	info string `json:"info"`
-	version string `json:"version"`
+
+type TrackInfo struct {
+	H_date string `json:"H_date"`
+	Pilot string `json:"pilot"`
+	Glider string `json:"glider"`
+	Glider_id string `json:"glider_id"`
+	Track_length json.Number `json:"track_length"`
 }
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
 
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
+ func apiHandler(w http.ResponseWriter, r *http.Request){
+ 	w.Header().Set("Content-Type", "application/json")
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
+ 	endTime := time.Now()
+
+ 	data := MetaData{
+ 		Uptime: fmt.Sprintf("P%dY%dM%dDT%dH%dM%dS",
+		endTime.Year() - startTime.Year(), endTime.Month() - startTime.Month(), endTime.Day() - startTime.Day(),
+		endTime.Hour() - startTime.Hour(), endTime.Minute() - startTime.Minute(), endTime.Second() - startTime.Second()),
+	 	Info: "Service for IGC tracks.",
+	 	Version: "V1",
+	 }
+
+	 jsonEnc := json.NewEncoder(w)
+	 print(jsonEnc.Encode(data))
+
+ }
+
+func igcHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	URL := strings.Split(r.URL.Path, "/")
+
+	if URL[5] != "" && r.Method == "GET"{
+
+		i, err := strconv.Atoi(URL[4])
+		if err != nil{
+			http.Error(w, "Something went wrong", http.StatusBadRequest)
+			return
+		}
+
+		field(w, r, i, URL[5])
+
+	} else if URL[4] != "" && r.Method == "GET"{
+
+		u, err := strconv.Atoi(URL[4])
+		if err != nil{
+			http.Error(w, "Something went wrong", http.StatusBadRequest)
+			return
+		}
+
+		for i := 0; i <= numTracks; i++{
+			if u == i{
+				trackinf[i] = TrackInfo{
+					H_date:       fmt.Sprintf(time.Now().String()),
+					Pilot:        "Gary",
+					Glider:       "G-" + strconv.Itoa(rand.Intn(500)),
+					Glider_id:    strconv.Itoa(rand.Intn(100)),
+					Track_length: json.Number(rand.Intn(10000)),
+				}
+				jsonEnc := json.NewEncoder(w)
+				print(jsonEnc.Encode(trackinf[i]))
+				return
+			}
+		}
+		http.Error(w, "NOT FOUND", http.StatusBadRequest)
+
+
+	} else if r.Method == "POST"{
+
+		type request struct {
+			Url string `json:"url"`
+		}
+
+		type response struct {
+			Id string `json:"id"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		var urlObj request
+
+		err := decoder.Decode(&urlObj)
+		if err != nil{
+			http.Error(w, "Something went wrong", http.StatusBadRequest)
+			return
+		}
+
+		tracks[numTracks] = urlObj.Url
+
+		resp := response{Id: string(numTracks)}
+
+		numTracks++
+
+		jsonEnc := json.NewEncoder(w)
+		print(jsonEnc.Encode(resp))
+
+
+	} else if r.Method == "GET"{
+
+		var array []int
+
+		for i := 0; i < numTracks; i++ {
+			array = append(array, i)
+		}
+
+		jsonEnc := json.NewEncoder(w)
+		print(jsonEnc.Encode(array))
+
+	} else {
+		http.Error(w, "Not a valid request", http.StatusBadRequest)
 		return
 	}
-	p, err := loadPage(title)
-	if err != nil {
-		http.Redirect(w, r, "/api/"+title, http.StatusFound)
+
+}
+
+func field (w http.ResponseWriter, r *http.Request, id int, field string){
+	w.Header().Set("Content-Type", "text/plain")
+	switch field {
+	case "h_date":
+		print(trackinf[id].H_date)
+	case "pilot":
+		print(trackinf[id].Pilot)
+	case "glider":
+		print(trackinf[id].Glider)
+	case "glider_id":
+		print(trackinf[id].Glider_id)
+	case "track_length":
+		print(trackinf[id].Track_length)
+	default:
+		http.Error(w, "Invalid Field", http.StatusBadRequest)
 		return
 	}
-	renderTemplate(w, "apiTemplate", p)
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.NotFound(w, r)
-		return "", errors.New("Invalid Page Title")
-	}
-	return m[2], nil // The title is the second subexpression.
-}
-
-func uptime() time.Duration{
-	return time.Since(startTime)
-}
 
 func main() {
-
 	startTime = time.Now()
+	tracks = make(map[int]string)
+	trackinf = make(map[int]TrackInfo)
+	numTracks = 0
 
 	s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
 	track, err := igc.ParseLocation(s)
@@ -84,14 +173,10 @@ func main() {
 		fmt.Errorf("Problem reading the track", err)
 	}
 
-	fmt.Printf("Pilot: %s, gliderType: %s, date: %s",
+	fmt.Printf("Pilot: %s, gliderType: %s, date: %s\n",
 		track.Pilot, track.GliderType, track.Date.String())
 
-	http.HandleFunc("/igcinfo/", viewHandler)
-
-
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
-
+	http.HandleFunc("/igcinfo/api/", apiHandler)
+	http.HandleFunc("/igcinfo/api/igc/", igcHandler)
+	http.ListenAndServe(":8080", nil)
 }
